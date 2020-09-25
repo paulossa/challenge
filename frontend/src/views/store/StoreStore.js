6f39b3e7da4226c59daacd930337cbb618487212
Paulo Santos <paulo@adianta.com.br>
2020-09-25 02:52:16 -0300

    Refatorando tela de loja'

--- frontend/src/views/store/StoreStore.js
+++ frontend/src/views/store/StoreStore.js
@@ -10,22 +10,24 @@ class StoreStore {
   constructor() {
     this.products = [];
     this.bankslip = [];
+    this.bankslipTotal = 0;
     this.promotions = {};
     this.bindActions(StoreActions);
   }
 
   getProducts(products) {
-    this.products = products.map(normalizeValue);
+    this.products = products;
   }
 
-  checkout(bankslip) {
-    this.bankslip = bankslip.map(normalizeValue);
+  checkout({bankslip, bankslip_total}) {
+    this.bankslip = bankslip;
+    this.bankslipTotal = bankslip_total;
   }
 
   getPromotions(promotions) {
     let temp_prom = {};
     for (const promotion of promotions) {
-      temp_prom[promotion.id] = `${promotion.type} - ${promotion.description}`;
+      temp_prom[promotion.id] = `${promotion.description}`;
     }
     this.promotions = temp_prom;
   }
